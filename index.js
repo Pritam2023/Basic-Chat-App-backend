ae37e1a27bbb8e4dac7ec54ab68a99161abee726
Pritam Chatterjee <74919879+Pritam2023@users.noreply.github.com>
2023-03-04 10:39:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URL, {
