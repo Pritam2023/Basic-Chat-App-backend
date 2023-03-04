08e4d63e3a3331d70eac0e397182c797a333572e
Pritam Chatterjee <74919879+Pritam2023@users.noreply.github.com>
2023-03-04 10:43:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const server = app.listen(PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://jade-salmon-robe.cyclic.app/auth",
+    origin: "http://localhost:3000",
     credentials: true,
   },
 });
